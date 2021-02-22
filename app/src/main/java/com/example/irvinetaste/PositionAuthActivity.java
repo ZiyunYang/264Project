c7a1820504c270d0b35d01038b48f1deffbd3c44
zhenyu zhang <hadleyzhang@gmail.com>
2021-02-21 20:24:52 -0800

    add something about configuration of UI of login and signup

---
+++ app/src/main/java/com/example/irvinetaste/PositionAuthActivity.java
@@ -0,0 +1,6 @@
+package com.example.irvinetaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PositionAuthActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/irvinetaste/SignupActivity.java
@@ -0,0 +1,6 @@
+package com.example.irvinetaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+}
