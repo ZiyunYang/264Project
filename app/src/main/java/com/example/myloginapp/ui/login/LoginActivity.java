2999d741a583c91e155a0b6f29a086054d625d1b
zhenyu zhang <hadleyzhang@gmail.com>
2021-02-21 18:07:33 -0800

    The login, signUp, Location auth.

--- app/src/main/java/com/example/myloginapp/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/myloginapp/ui/login/LoginActivity.java
@@ -46,6 +46,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onChanged(@Nullable LoginFormState loginFormState) {
                 if (loginFormState == null) {
+
                     return;
                 }
                 loginButton.setEnabled(loginFormState.isDataValid());
