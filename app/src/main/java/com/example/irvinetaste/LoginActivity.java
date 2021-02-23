95df3e600d4a068f73d6836998da24f4acec362a
zhenyu zhang <hadleyzhang@gmail.com>
2021-02-23 14:06:03 -0800

    add more configuration setting into build.gradle

--- app/src/main/java/com/example/irvinetaste/LoginActivity.java
+++ app/src/main/java/com/example/irvinetaste/LoginActivity.java
@@ -1,7 +1,35 @@
 package com.example.irvinetaste;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.login);
+        EditText usernameText = (EditText) findViewById(R.id.username);
+        EditText passwordText = (EditText) findViewById(R.id.password);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
 
 }
---
+++ app/src/main/java/com/example/irvinetaste/Utils/MongoDBUtil.java
@@ -0,0 +1,133 @@
+package com.example.irvinetaste.Utils;
+
+import com.mongodb.DBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+
+import org.bson.Document;
+
+import java.util.List;
+
+/**
+ * @author zhangzhenyu
+ * @date 2021/2/23 11:17
+ * @version 1.0
+ */
+public class MongoDBUtil {
+
+    MongoClient mongoClient = null;
+    MongoDatabase db = null;
+
+
+    public MongoDBUtil(String dbName){
+
+        String databaseUser = "root";
+        String databasePassword = "uci123456";
+        String host = "cluster0.v64ra.mongodb.net/myFirstDatabase";
+
+        try{
+            mongoClient= MongoClients.create("mongodb+srv://<root>:<uci123456>@cluster0.v64ra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            db= mongoClient.getDatabase(dbName);
+//            System.out.println(db.getName());
+            System.out.println("Connect tot database successfully");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getCollection(String collName){
+        MongoCollection<Document> collection = db.getCollection(collName);
+
+        System.out.println("the number of result is " + collection.countDocuments());
+
+        FindIterable<Document> findIterable = collection.find();
+        MongoCursor<Document> mongoCursor = findIterable.iterator();
+        while(mongoCursor.hasNext()){
+            System.out.println(mongoCursor.next());
+        }
+
+    }
+
+    /**
+     * Create a blanket collection.
+     * @param collName :collection name.
+     */
+    public void createCollection(String collName)
+    {
+        db.createCollection(collName);
+        //TODO It has error
+    }
+
+    /**
+     * Insert dbObject into collection.
+     * @param dbObject
+     * @param collName
+     */
+    public void insert(DBObject dbObject, String collName)
+    {
+
+    }
+
+    /**
+     * Insert dbObject list into collection.
+     * @param dbObjects
+     * @param collName
+     */
+    public void insertBatch(List<DBObject> dbObjects, String collName)
+    {
+
+    }
+
+    /**
+     * Delete data By Id.
+     * @param id
+     * @param collName
+     * @return
+     */
+    public int deleteById(String id,String collName)
+    {
+        return 0;
+    }
+
+    /**
+     * Delete data By Condition.
+     * @param dbObject
+     * @param collName
+     * @return
+     */
+    public int deleteByDbs(DBObject dbObject,String collName)
+    {
+        return 0;
+    }
+
+    /**
+     * Update Data.
+     * @param find
+     * @param update
+     * @param upsert
+     * @param multi
+     * @param collName
+     * @return
+     */
+    public int update(DBObject find,DBObject update,boolean upsert,boolean multi,String collName)
+    {
+        return 0;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        MongoDBUtil mongoDBUtil = new MongoDBUtil("irvinetaste");
+
+        mongoDBUtil.getCollection("user");
+    }
+
+
+}
