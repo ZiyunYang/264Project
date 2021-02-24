53163c0ef8d2278cecd7406e82744e5fd35afb4a
Zerichen <cz1296@nyu.edu>
2021-02-23 18:38:00 -0800

    finished part of userprofile

--- app/src/main/java/com/example/irvinetaste/UserProfileActivity.java
+++ app/src/main/java/com/example/irvinetaste/UserProfileActivity.java
@@ -3,6 +3,10 @@ package com.example.irvinetaste;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.irvinetaste.utils.MongoDBUtil;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
+        MongoDBUtil dbUtil = new MongoDBUtil("irvinetaste");
+        User user = dbUtil.getCollection("user");
+        EditText usrname = findViewById(R.id.editTextTextPersonName);
+        EditText usrphone = findViewById(R.id.editTextPhone);
+        usrname.setText(user.getUsername());
+        usrphone.setText(user.getPhoneNum());
+    }
+
+    public void goToBookmark(View view) {
+
     }
 }
