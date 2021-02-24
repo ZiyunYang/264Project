a16d396c16c2cc383296f1df00fefe4fcd562e93
zhenyu zhang <hadleyzhang@gmail.com>
2021-02-24 14:02:40 -0800

    add PhoneNumberUtils to check the validity of input number

--- app/src/main/java/com/example/irvinetaste/ForgetAccountActivity.java
+++ app/src/main/java/com/example/irvinetaste/ForgetAccountActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//TODO
 public class ForgetAccountActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/irvinetaste/utils/PhoneNumberUtils.java
+++ app/src/main/java/com/example/irvinetaste/utils/PhoneNumberUtils.java
@@ -13,7 +13,7 @@ public class PhoneNumberUtils {
      * regex match phone number:
      */
     public static boolean checkTel(String tel){
-        Pattern p = Pattern.compile("^[1][3,4,5,7,8,9][0-9]{9}$");
+        Pattern p = Pattern.compile("^[1][3,4,5,7,8,9][0-9]{10}$");
         Matcher matcher = p.matcher(tel);
         return matcher.matches();
     }
